92d8402ec1e2de273b41db03df60b149b10dec71
LuciaPerazzolo <luciaperazzolo22@gmail.com>
2026-06-03 23:51:16 -0300

    - Se creó la carpeta 'vistas' dentro del paquete principal 'com.cyber' para alojar los componentes de la interfaz gráfica. - Se reubicó el archivo principal 'SistemaGestionCyber.java' dentro del paquete raíz para respetar la convención de paquetes de Maven. - Se corrigió la declaración del package en el archivo principal para solucionar los errores de compilación.

--- src/main/java/com/cyber/SistemaGestionCyber.java
+++ src/main/java/com/cyber/SistemaGestionCyber.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 
 public class SistemaGestionCyber {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         
         Connection conexion = ConexionBD.conectar();  //Intentamos conectarnos a la base de datos
 
---
+++ src/main/java/com/cyber/vistas/ClienteVista.java
@@ -0,0 +1,5 @@
+package com.cyber.vistas;
+
+public class ClienteVista {
+    
+}
