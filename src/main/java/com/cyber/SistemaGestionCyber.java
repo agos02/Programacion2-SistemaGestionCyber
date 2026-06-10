c898e50aa3a873ff14397bb9ff7b2a94d2d3e7d9
Giovanna M. <210918197+STARKgazing@users.noreply.github.com>
2026-06-10 16:14:18 -0300

    Update SistemaGestionCyber.java (Agregar conecciones a los paquetes de computadoras)

--- src/main/java/com/cyber/SistemaGestionCyber.java
+++ src/main/java/com/cyber/SistemaGestionCyber.java
@@ -1,22 +1,37 @@
 package com.cyber;
 
 import com.cyber.conexion.ConexionBD;
+import com.cyber.dao.ComputadoraDAO;
+import com.cyber.modelos.Computadora;
+
 import java.sql.Connection;
 
 public class SistemaGestionCyber {
 
-    public static void main(String[] args) throws Exception{
-        
-        Connection conexion = ConexionBD.conectar();  //Intentamos conectarnos a la base de datos
+    public static void main(String[] args) {
 
-        if (conexion != null) {     //Verificamos si la conexión funcionó
+        Connection conexion = ConexionBD.conectar();
+
+        if (conexion != null) {
+
             System.out.println("La conexión funciona correctamente.");
-            
-            ConexionBD.cerrar(conexion); //Una vez probado, cerramos la conexión inmediatamente
-            
-        } else {
-            System.out.println("No se pudo conectar a la base de datos.");
-        }
 
+            ConexionBD.cerrar(conexion);
+
+            ComputadoraDAO dao = new ComputadoraDAO();
+
+            dao.insertar(new Computadora(0, 1, "Libre"));
+            dao.insertar(new Computadora(0, 2, "Libre"));
+            dao.insertar(new Computadora(0, 3, "Libre"));
+
+            System.out.println(
+            "Cantidad: " + dao.listar().size()
+            );
+
+        } else {
+
+            System.out.println("No se pudo conectar a la base de datos.");
+
+        }
     }
 }
