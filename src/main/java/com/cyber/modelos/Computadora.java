a7d267ba69d570fb2a93993adf800d7429489132
Giovanna M. <210918197+STARKgazing@users.noreply.github.com>
2026-06-10 16:15:23 -0300

    Update Computadora.java (Agregar variables, setters y geeters)

--- src/main/java/com/cyber/modelos/Computadora.java
+++ src/main/java/com/cyber/modelos/Computadora.java
@@ -9,5 +9,37 @@ package com.cyber.modelos;
  * @author rosch
  */
 public class Computadora {
+    int id_computadora;
+    int numero_pc;
+    String estado;
     
+    public Computadora(int id_computadora, int numero_pc, String estado) {
+        this.id_computadora = id_computadora;
+        this.numero_pc = numero_pc;
+        this.estado = estado;
+    }
+    
+    public int getIdComputadora(){
+        return this.id_computadora;
+    }
+    
+    public int getNumeroPC(){
+        return this.numero_pc;
+    }
+    
+    public String getEstado(){
+        return this.estado;
+    }
+    
+    public void setIdComputadora(int id_computadora){
+        this.id_computadora = id_computadora;
+    }
+    
+    public void setNumeroPC(int numero_pc){
+        this.numero_pc = numero_pc;
+    }
+    
+    public void setEstado(String Esatdo){
+        this.estado = estado;
+    }
 }
