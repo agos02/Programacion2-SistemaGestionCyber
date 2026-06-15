18f333b178d66d761f538a1de937246fd4d63061
Giovanna M. <210918197+STARKgazing@users.noreply.github.com>
2026-06-15 05:13:01 -0300

    Update SistemaGestionCyber.java (Abre vista de Gestion de Computadoras. EDITAR LUEGO PARA ABRIR LA VISTA INICIAL DE LA APP)

--- src/main/java/com/cyber/SistemaGestionCyber.java
+++ src/main/java/com/cyber/SistemaGestionCyber.java
@@ -1,37 +1,26 @@
 package com.cyber;
 
 import com.cyber.conexion.ConexionBD;
+import com.cyber.controladores.ComputadoraControlador;
 import com.cyber.dao.ComputadoraDAO;
 import com.cyber.modelos.Computadora;
+import com.cyber.vistas.ComputadoraVista;
 
 import java.sql.Connection;
 
 public class SistemaGestionCyber {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
 
-        Connection conexion = ConexionBD.conectar();
+        ComputadoraVista vista =
+            new ComputadoraVista();
+        
+        ComputadoraDAO dao =
+            new ComputadoraDAO();
 
-        if (conexion != null) {
+        ComputadoraControlador controlador =
+            new ComputadoraControlador(vista, dao);
 
-            System.out.println("La conexión funciona correctamente.");
-
-            ConexionBD.cerrar(conexion);
-
-            ComputadoraDAO dao = new ComputadoraDAO();
-
-            dao.insertar(new Computadora(0, 1, "Libre"));
-            dao.insertar(new Computadora(0, 2, "Libre"));
-            dao.insertar(new Computadora(0, 3, "Libre"));
-
-            System.out.println(
-            "Cantidad: " + dao.listar().size()
-            );
-
-        } else {
-
-            System.out.println("No se pudo conectar a la base de datos.");
-
-        }
+        vista.setVisible(true);
     }
 }
