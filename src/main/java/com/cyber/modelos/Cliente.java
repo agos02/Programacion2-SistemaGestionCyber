134f20309433d3e380ea2521de78eba2a3563320
thomasss070 <thomas07gimenez@gmail.com>
2026-06-15 23:06:17 -0300

    Revert "Feature/reportes"

--- src/main/java/com/cyber/modelos/Cliente.java
+++ src/main/java/com/cyber/modelos/Cliente.java
@@ -1,51 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.cyber.modelos;
 
 /**
- * Clase modelo que representa la tabla 'clientes' en código.
- * Contiene atributos, constructores, getters y setters.
+ *
+ * @author rosch
  */
-public class Cliente {
-
-    // Atributos
-    private int idCliente;
-    private String nombre;
-    private String dni;
-    private String telefono;
-
-    // Constructor vacío
-    public Cliente() {}
-
-    // Constructor con parámetros
-    public Cliente(int idCliente, String nombre, String dni, String telefono) {
-        this.idCliente = idCliente;
-        this.nombre = nombre;
-        this.dni = dni;
-        this.telefono = telefono;
-    }
-
-    // Getters
-    public int getIdCliente() {
-        return idCliente; 
-    }
-    public String getNombre() {
-        return nombre; 
-    }
-    public String getDni() {
-        return dni; 
-    }
-    public String getTelefono() { return telefono; }
-
-    // Setters
-    public void setIdCliente(int idCliente) {
-        this.idCliente = idCliente; 
-    }
-    public void setNombre(String nombre) { 
-        this.nombre = nombre; 
-    }
-    public void setDni(String dni) { 
-        this.dni = dni; 
-    }
-    public void setTelefono(String telefono) { 
-        this.telefono = telefono; 
-    }
+public class Cliente { ////clase que representa la tabla clientes en código.
+    
 }
